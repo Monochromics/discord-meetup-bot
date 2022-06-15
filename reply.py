cc21a3de5af1ca0679b45d48451f2835f7240eaf
Kyler Hornor <kylerhornor@yahoo.com>
2022-06-14 23:16:36 -0500

    fix ordering

--- reply.py
+++ reply.py
@@ -1,9 +1,13 @@
-# This example requires the 'message_content' privileged intent to function.
+#!/usr/bin/python3
 
+# This example requires the 'message_content' privileged intent to function.
 import discord
 from discord.ext import commands
 import scrape
 import yaml
+
+with open('config.yaml') as f:
+    config = yaml.safe_load(f)
 
 intents = discord.Intents.default()
 intents.messages = True
@@ -13,9 +17,5 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.command()
 async def events(ctx):
     await ctx.send(scrape.scrape())
-
-
-with open('config.yaml') as f:
-    config = yaml.safe_load(f)
 
 bot.run(config['token'])
